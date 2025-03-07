bae139546990aeefc81baaea5acfa2fac0e31634
Valente <valentecardenas30@gmail.com>
2025-03-06 21:01:00 -0600

    prueba de integracion a la rama principal

---
+++ MODELO1/Base de datos/Database.sql
@@ -0,0 +1,58 @@
+DROP DATABASE IF EXISTS db_modelo1;
+CREATE DATABASE IF NOT EXISTS db_modelo1;
+
+USE db_modelo1;
+
+CREATE TABLE Usuario (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(80) NOT NULL,
+    apellido_pat VARCHAR(20) NOT NULL,
+    apellido_mat VARCHAR(20) NOT NULL,
+    correo VARCHAR(150) NOT NULL,
+    telefono VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Inventario (
+    id_inventario INT AUTO_INCREMENT PRIMARY KEY,
+    id_sucursal INT,
+    FOREIGN KEY (id_sucursal) REFERENCES Sucursal(id_sucursal)
+);
+
+CREATE TABLE Autor (
+    id_autor INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(250) NOT NULL,
+    nacionalidad VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Libro (
+    id_libro INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(250) NOT NULL,
+    editorial VARCHAR(100) NOT NULL,
+    id_autor INT,
+    FOREIGN KEY (id_autor) REFERENCES Autor(id_autor)
+);
+
+CREATE TABLE Prestamo (
+    id_prestamo INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    id_libro INT NOT NULL,
+    fecha_prestamo DATE NOT NULL,
+    fecha_devolucion DATE NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
+    FOREIGN KEY (id_libro) REFERENCES Libro(id_libro)
+);
+
+CREATE TABLE Administrador (
+    id_administrador INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
+);
+
+CREATE TABLE Sucursal (
+    id_sucursal INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    direccion VARCHAR(200) NOT NULL,
+    id_administrador INT,
+    FOREIGN KEY (id_gerente) REFERENCES Usuario(id_usuario)
+    FOREIGN KEY (id_administrador) REFERENCES Administrador(id_administrador)
+);
